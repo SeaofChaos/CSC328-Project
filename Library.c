fa0722c390a075ed5d5cbdc4c1db0b34ad837037
ckiri354 <95324752+ckiri354@users.noreply.github.com>
2021-12-02 17:46:38 -0500

    Update Library.c

--- Library.c
+++ Library.c
@@ -1,1 +1,27 @@
-//library code file
+
+//check that port given is valid
+int checkPort(int givenPort)
+{
+    if(givenPort<1024||givenPort>49151)
+        return 7011;
+ 
+    else 
+        return givenPort;
+}
+
+
+int defaultPort()
+{
+return 7011;
+}
+
+void socketFail()
+{
+		perror("Socket call failed");
+		exit(1);
+}
+
+char *  receiveMsg(char *received)
+{
+  
+}
