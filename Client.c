47f996a87acb98acf13f507a2a2b61195d4af8e3
SeaofChaos <46305438+SeaofChaos@users.noreply.github.com>
2021-12-02 15:33:13 -0500

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////
 // 	Author: 		Ryan Quinn
 // 	Major: 			Computer Science
-//	Creation Date:		November 30, 2021
+//	Creation Date:	November 30, 2021
 //	Due Date: 		December 2, 2021
 // 	Course: 		CSC328 010
 //	Professor:		Dr. Frye
@@ -44,22 +44,22 @@ int main(int argc, char* argv[]){
 	struct addrinfo hints, *result;
 	
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; 
-	hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_UNSPEC; 
+    hints.ai_socktype = SOCK_STREAM;
 	
 	//get IP address from hostname
-	if ((s = getaddrinfo(argv[1], port, &hints , &result)) != 0) 
-	{
-    		perror("Error getting dns");
-    		exit(1);
-	}
+    if ((s = getaddrinfo(argv[1], port, &hints , &result)) != 0) 
+    {
+        perror("Error getting dns");
+        exit(1);
+    }
 	
 	//create socket
 	if ((sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == -1)
-	{
+    {
 		perror("Socket call failed");
 		exit(1);
-	}
+    }
 	
 	//connect socket
 	if (connect(sockfd, result->ai_addr, result->ai_addrlen) == -1)
@@ -70,14 +70,15 @@ int main(int argc, char* argv[]){
 	freeaddrinfo(result);
 	
 	char *received = malloc(sizeof(char));
+	char *toSend = "a";
 	//char *received = "hello";
 	
 	if (recv(sockfd, received, sizeof(received), 0) < 0){
 		printf("Unable to read data from server");
 	}
-	//if (send(sockfd, received, sizeof(received), 0) < 0){
-	//	printf("Unable to send data to server);
-	//}
+	if (send(sockfd, toSend, sizeof(toSend), 0) < 0){
+		printf("Unable to send data to server");
+	}
 	
 	printf("Received from server: %c\n", received);
 	//printf("Sent to server: %s\n", received);
