538f47612034ad9889682cd99ca41dacb93886c6
SeaofChaos <46305438+SeaofChaos@users.noreply.github.com>
2021-12-03 02:42:04 -0500

    Update Client.c
    It should have most of the functionality needed to run the client. There are a few things I need to tweak to make it more user-friendly if I have more time.

--- Client.c
+++ Client.c
@@ -14,7 +14,7 @@
 //                  inputs and outputs of information to and
 //                  from the server.
 //
-//	How to compile:	
+//	How to compile:	make
 /////////////////////////////////////////////////////////////
 
 #include <stdlib.h>
@@ -27,19 +27,57 @@
 
 #include "Library.c"
 
+void getNickName(int sockfd){
+	char nickname[50];
+	char response[6];
+	
+	printf("Choose a nickname (under 50 characters): ");
+	scanf("%s", nickname);
+	
+	if (send(sockfd, nickname, sizeof(nickname), 0) < 0){
+		printf("Unable to send data to server\n");
+	}
+	if (recv(sockfd, response, sizeof(response), 0) < 0){
+		printf("Unable to read data from server\n");
+	}
+	
+	if (strcmp(response, "READY") != 0){
+		printf("\nNickname is not unique.\nPlease enter a new nickname.\n\n");
+		getNickName(sockfd);
+	}
+}
+
+void getRPS(int sockfd){
+	char choice[8];
+	printf("Rock, paper, or scissors (type in lowercase): ");
+    scanf("%s", choice);
+
+    if (strcmp(choice, "rock") != 0 && 
+			strcmp(choice, "paper") != 0 && 
+			strcmp(choice, "scissors")){
+				
+		printf("\nInvalid choice, please enter \"rock\", \"paper\", or \"scissors\"\n\n");
+		getRPS(sockfd);
+	}
+	
+	if (recv(sockfd, choice, sizeof(choice), 0) < 0){
+		printf("Unable to read data from server\n");
+	}
+}
+
 int main(int argc, char* argv[]){
 	
 	if ((argc < 2) || (argc > 3)){	//proper usage check
 		printf("Usage: %s hostname port(optional)\n", argv[0]);
 		return 0;
-	}
+	} 
 	
 	char* type = argv[1];
 	char* port = "7006";
 	
 	if (argc == 3){	//is port specified?
 		port = argv[2];
-	}
+	} 
 	
 	//declarations
 	int sockfd, s;
@@ -71,22 +109,47 @@ int main(int argc, char* argv[]){
 	}
 	freeaddrinfo(result);
 	
-	char received[50];
-	//char *received;
-	char *toSend = "hello world";
-	//char *received = "hello";
 	
-	if (recv(sockfd, received, sizeof(received), 0) < 0){
-		printf("Unable to read data from server");
+	char keepPlaying = 'y';
+	
+	while (keepPlaying == 'y'){  //game loop
+		char received[50];
+		char *toSend[50];
+		
+		getNickName(sockfd); //gets a unique nickname
+		
+		//keep running until "SCORE" is received
+		while (strcmp(received, "SCORE") != 0){
+			//waiting for "GO"
+			while(1){
+				if (recv(sockfd, &received, sizeof(received), 0) < 0){
+					printf("Unable to read data from server\n");
+				}
+				if (strcmp(received, "GO") == 0) //received "GO"
+					break;
+			}
+			
+			getRPS(sockfd);	//gets and sends an inputted move to server
+			
+			//get response from server, where "SCORE" will stop game
+			if (recv(sockfd, &received, sizeof(received), 0) < 0){
+				printf("Unable to read data from server\n");
+			}
+		}
+		
+		printf("Would you like to play again? (y/n): ");
+		scanf("%c", toSend);
+		
+		if (send(sockfd, toSend, 1, 0) < 0){
+			printf("Unable to send data to server\n");
+		}
+		
+		//receives char for keepPlaying, 'y' being to keep going, 'n' to stop
+		if (recv(sockfd, &keepPlaying, 1, 0) < 0){
+			printf("Unable to read data from server\n");
+		}
+		//printf("Received from server: %s\n", received);
 	}
-	if (send(sockfd, toSend, 50, 0) < 0){
-		printf("Unable to send data to server");
-	}
-	
-	printf("Received from server: %s\n", received);
-	
-	
-	
 	close(sockfd);
 	
 	return 0;
