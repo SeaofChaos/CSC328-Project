17f34a0f96b49f78f99d621365fa7bcd36c60cd4
SeaofChaos <46305438+SeaofChaos@users.noreply.github.com>
2021-12-04 23:23:42 -0500

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,379 +1,156 @@
-// server code file
+
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <signal.h>
-#include <errno.h>
-#include <stdio.h>
+#include <netdb.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <sys/wait.h>
- 
+
 #include "Library.c"
 
-#define SIZE sizeof(struct sockaddr_in)
-#define MAXLINE 512
-#define READ      0
-#define WRITE     1
-
-static int received = 0;
-
-void waitProcess(int signal){
-	if (signal == SIGUSR1)
-    {
-        received = 1;
-    }
+char* tolowerString(char* str){
+	char *lower = malloc(sizeof(str));
+	for (int i=0; i<sizeof(str); ++i)
+		lower[i] = tolower(str[i]);
+	return lower;
 }
 
-int main(int argc, char **argv)
-{
-  char *uniq;
-  char nick[50];
-  char *ready = "READY";
-  int PORT = 7008;
-  int sockfd, rv;
-  int numChild = 0;
-  
-  if (argc < 2) //usage clause
-    {
-      printf("\nusage: %s <number of rounds> <port number>(optional) \n\n", argv[0]);
-      exit(1);
-    }
-  int numRounds = atoi(argv[1]); // set number of rounds 
-  printf("numRounds: %d\n", numRounds);
-  if (argv[2] != NULL) // Set port if user entered 2nd CLA
-    {
-      PORT = atoi(argv[2]);
-    }
-  
-  struct sockaddr_in server = {AF_INET, htons(PORT) ,INADDR_ANY};
-  
-  // set up the transport end point
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-      perror("socket call failed");
-      exit(-1);
-    }   // end if
-  
-  // bind and address to the end point
-  if (bind(sockfd, (struct sockaddr *)&server, SIZE) == -1)
-    {
-      perror("bind call failed");
-      exit(-1);
-    }   // end if bind
-  
-  // start listening for incoming connections
-  if (listen(sockfd, 5) == -1)
-    {
-      perror("listen call failed");
-      exit(-1);
-    }   // end if
+void getNickName(int sockfd){
+	char nickname[50];
+	char response[50];
 	
-	for (;;)
-    {
-		signal(SIGUSR1,waitProcess);
-		int scoreP1 = 0, scoreP2 = 0;
-		char uniq[50];
-		char nick1[50], nick2[50];
-		int pid;
-		int newsockfd;
-		int p2c[2], c2p[2];
-		
-		if (pipe(p2c) < 0) {
-			perror("Error creating pipe for parent to child:");
-			exit(-1);
-		}   // end if
+	printf("Choose a nickname (under 50 characters): ");
+	scanf("%s", nickname);
+	
+	if (send(sockfd, nickname, sizeof(nickname), 0) < 0){
+		printf("Unable to send data to server\n");
+	}
+	if (recv(sockfd, response, sizeof(response), 0) < 0){
+		printf("Unable to read data from server\n");
+	}
+	
+	printf("\nReady or retry: %s\n", response);
+	
+	if (strcmp(response, "READY") != 0){
+		printf("\nNickname is not unique.\nPlease enter a new nickname.\n\n");
+		getNickName(sockfd);
+	}
+}
 
-		// Create a second pipe to send data child to  parent
-		if (pipe(c2p) < 0) {
-			perror("Error creating pipe for child to parent:");
-			exit(-1);
-		}   // end if
-		
-		if ((pid = fork()) == -1)
-		{
-			perror("fork failed");
-			exit(-1);
-		}   // end if
-		if (pid != 0){
-			if ((pid = fork()) == -1)
-			{
-				perror("fork failed");
-				exit(-1);
-			}   // end if
-		}
-		if (pid == 0) // CHILD Process
-		{
-			// accept connection
-			newsockfd = accept(sockfd, NULL, NULL);
-			if (newsockfd == -1)
-			{
-				if (errno==EINTR) continue;
-				perror("accept call failed");
-				exit(-1);
-			}   // end if
-			
-			printf("Connection received\n");
-			
-			char nick[50];
-			
-			close(p2c[WRITE]); //close unneeded pipes
-			close(c2p[READ]);
-			
-			rv = send(newsockfd, ready, sizeof(ready), 0); // send ready string 
-			if (rv < 0)
-				perror("Error sending to socket1");
-		  
-			rv = recv(newsockfd, nick, sizeof(nick),0); // receive nickname from client
-			if (rv < 0)
-				perror("Error receiving from socket2");
-		  
-			printf("Nickname received in child: %s\n", nick);
-			
-			rv = write(c2p[WRITE], &nick, sizeof(nick));  // write nickname to parent
-			if (rv < 0)
-				perror("Error writing to parent pipe2");
-			read(p2c[READ], &uniq, sizeof(uniq)); //read uniqueness response
-			if (rv < 0)
-				perror("Error reading from parent pipe2");
-			
-			printf("uniq: %s\n", uniq);
-			
-			printf("value: %d\n", strcmp(uniq, "READY"));
-			
-			while (strcmp(uniq, "READY") != 0)
-			{
-				rv = send(newsockfd, uniq, sizeof(uniq), 0); // send uniq of RETRY response to client
-				if (rv < 0)
-					perror("Error sending to socket3");
-				rv = recv(newsockfd, nick, sizeof(nick),0); // receive nickname from client
-				if (rv < 0)
-					perror("Error receiving from socket4");
-				rv = write(c2p[WRITE], &nick, sizeof(nick));  // write nickname to parent
-				if (rv < 0)
-						perror("Error writing to pipes5");
-				rv = read(p2c[READ], &uniq, sizeof(uniq)); //read uniqueness response
-				if (rv < 0)
-						perror("Error reading from pipes6");
-				//printf("About to send to client: READY\n");
-				
-				rv = send(newsockfd, uniq, sizeof(uniq), 0); // send uniq of READY response to client
-				if (rv < 0)
-					perror("Error sending to socket7");
-			}
-			//sleep(0.5);
-			/*rv = write(c2p[WRITE], &uniq, sizeof(uniq));  // write nickname to parent
-			if (rv < 0)
-				perror("Error sending to socket7");
-			*/
-			//i don't know why this printf doesn't print, but we don't talk worry about it
-			//because it works :)
-			printf("About to send to client: %s\n", uniq);
-			
-			rv = send(newsockfd, uniq, sizeof(uniq), 0); // send uniq of READY response to client
-			if (rv < 0)
-				perror("Error sending to socket7");
-			
-			printf("Got past sending READY to client\n");
-			
-			//writing message back to parent that one child has received message
-			rv = write(c2p[WRITE], "RECEIVED", 9);  // write nickname to parent
-			if (rv < 0)
-					perror("Error writing to pipes5");
-			
-			//play game
-            //for (int x = 0; x < numRounds; x++) {
-				// send GO to client - Ready for RPS selection
-                char* goString = "GO";
-				printf("About to send 'GO'\n");
-				rv = send(newsockfd, goString, sizeof(goString), 0); 
-				printf("Sent 'GO'\n");
-				
-				//get choice from CLIENT
-                char choice[50];
-                rv = recv(newsockfd, choice, sizeof(choice), 0); // receive choice from client
-				printf("Choice received from client: %s\n", choice);
-				printf("uniq before reading PLAYER#: %s\n", uniq);
-				//read PLAYER#
-				rv = write(c2p[WRITE], "START", 6); //pipe choice to parent
-				rv = read(p2c[READ], &uniq, sizeof(uniq)); //read PLAYER# from parent
-				
-				printf("Read from parent pipe: %s\n", uniq);
-				
-				rv = write(c2p[WRITE], &nick, sizeof(nick)); //pipe choice to parent
-				printf("goopy\n");
-				rv = write(c2p[WRITE], &choice, sizeof(choice)); //pipe choice to parent
-				printf("mommy\n");
-				
-				//while (rv > 0) rv = read(p2c[READ], &uniq, sizeof(uniq));
-				//while (received != 1);
-				
-				//rv = read(p2c[READ], &uniq, sizeof(uniq)); //make sure both children are done
-				//printf("daddy\n");
-				
-				/*if (strcmp(uniq, nick)){
-					
-				}*/
-				
-				//if (x < numRounds)
-					//send(newsockfd, "NEXT ROUND", 11, 0);
-				//else{
-					send(newsockfd, "SCORE", 6, 0);
-					//break;
-				//}
-				//printf("ahhh\n");
-				//rv = read(p2c[READ], &uniq, sizeof(uniq)); //make sure both children are done
-            //}
-			
-			close(p2c[READ]); //close unneeded pipes
-			close(c2p[WRITE]);
-			close(sockfd);
-			close(newsockfd);
-			exit(0);
-		}
-	  	else  // PARENT Process 
-		{
-			close(p2c[READ]); //close unneeded pipes
-			close(c2p[WRITE]);
-			
-			char *toSend = malloc(sizeof(char)*10);
-			int isReady = 0;
-			while (isReady != 1)
-			{
-				rv = read(c2p[READ], &nick1, sizeof(nick1)); //read nickname from client 1
-				if (rv < 0)
-					perror("Error reading from pipes12");
-				
-				rv = read(c2p[READ], &nick2, sizeof(nick2)); // read nickname from client 2
-				if (rv < 0)
-					perror("Error reading from pipes13");
-				
-				printf("Nick1 received in parent: %s\n", nick1);
-				printf("Nick2 received in parent: %s\n", nick2);
-				
-				rv = strcmp(nick1, nick2); // check uniqueness 
-				if (rv == 0) // send retry if not unique
-				{
-					toSend = "RETRY";
-					for (int i=0; i<2; i++){
-						printf("About to send: %s\n", toSend);
-						rv = write(p2c[WRITE], toSend, sizeof(toSend));
-						if (rv < 0)
-							perror("Error writing to pipes14");
-					}
-				}
-				else //send ready if unique
-				{
-					toSend = "READY";
-					isReady = 1;
-					for (int i=0; i<2; i++){
-						//printf("About to send: %s\n", toSend);
-						rv = write(p2c[WRITE], toSend, sizeof(toSend));
-						if (rv < 0)
-							perror("Error writing to pipes15");
-						rv = read(c2p[READ], &nick1, sizeof(nick1)); //read nickname from client 1
-					}
-				}
-			}
-			
-			//for (int x = 0; x < numRounds; x++) {
-					char c1[10], c2[10];
-					char player1[50], player2[50];
-					//int childNum = 1;
-					
-				for (int i=0; i<2; ++i){
-					rv = read(c2p[READ], uniq, sizeof(uniq));
-					if (i == 0){
-						printf("getting player1 information:\n");
-						printf("before PLAYER1 write\n");
-						rv = write(p2c[WRITE], "PLAYER1", 8);
-						printf("Read player1 nick\n");
-						rv = read(c2p[READ], player1, sizeof(player1));
-						
-						printf("Should be player1: %s\n", player1);
-						rv = read(c2p[READ], c1, sizeof(c1));
-						
-						printf("Should be player1's move: %s\n", c1);
-					}
-					if (i == 1){
-						rv = read(c2p[READ], uniq, sizeof(uniq));
-						printf("getting player2 information:\n");
-						printf("before PLAYER2 write\n");
-						rv = write(p2c[WRITE], "PLAYER2", 8);
-						printf("Read player2 nick\n");
-						rv = read(c2p[READ], player2, sizeof(player2));
-						
-						printf("Should be player2: %s\n", player2);
-						rv = read(c2p[READ], c2, sizeof(c2));
-						printf("Should be player2's move: %s\n", c2);
-					}
-					//childNum = 2;
-				}
-				
-				//kill(pid, SIGUSR1);
-				
-				printf("Player1 move: %s\nPlayer2 move: %s\n", c1, c2);
-				
-                int ref = decide(c1, c2);
-				printf("ref: %d\n", ref);
-                switch (ref) { //scoring
-                    case 0:{
-                        printf("It's a tie.\n");
-						break;
-					}
-                    case 1:{
-						printf("player1, nick: %s, %s\n", player1, nick);
-						if (strcmp(player1, nick1) == 0){
-							printf("Player1 wins\n");
-							scoreP2++;
-						}
-						else{
-							scoreP1++;
-							printf("Player2 wins\n");
-						}
-						break;
-					}
-                    case 2:{
-						printf("player1, nick: %s, %s\n", player1, nick1);
-                        if (strcmp(player1, nick1) == 0){
-							scoreP1++;
-							printf("Player2 wins\n");
-						}
-						else{
-							scoreP2++;
-							printf("Player1 wins\n");
-						}
-						break;
-					}
-                    case 4:
-                        printf("Round Canceled: Invalid Entry");
-                        break;
-                }
-				printf("player1 score: %d\nplayer2 score: %d\n", scoreP1, scoreP2);
-				/*if (scoreP1 > scoreP2)
-					printf("Player1 wins\n");
-				else if (scoreP1 == scoreP2)
-					printf("Tie\n");
-				else
-					printf("Loss\n");*/
-				//rv = write(p2c[WRITE], "FINISHED", 9);
-            //}
-			close(p2c[WRITE]);
-			close(c2p[READ]);
-		} //end else
-		if (wait(NULL) < -1)
-			perror("Error in wait: ");
-		if (wait(NULL) < -1)
-			perror("Error in wait: ");
-    } //end for (game loop)
+void getRPS(int sockfd){
+	char choice[10];
+	printf("Rock, paper, or scissors (type in lowercase): ");
+    scanf("%s", choice);
+	tolowerString(choice);
+	printf("Inputted choice in lowercase: %s\n", choice);
 	
+    if (strcmp(choice, "rock") != 0 && 
+			strcmp(choice, "paper") != 0 && 
+			strcmp(choice, "scissors") != 0){
+		
+		printf("\nInvalid choice, please enter \"rock\", \"paper\", or \"scissors\"\n\n");
+		getRPS(sockfd);
+	}
+	
+	if (send(sockfd, choice, sizeof(choice), 0) < 0){
+		printf("Unable to read data from server\n");
+	}
+	
+	/*if (recv(sockfd, choice, sizeof(choice), 0) < 0){
+		printf("Unable to read data from server\n");
+	}*/
+}
+
+int main(int argc, char* argv[]){
+	
+	if ((argc < 2) || (argc > 3)){	//proper usage check
+		printf("Usage: %s hostname port(optional)\n", argv[0]);
+		return 0;
+	} 
+	
+	char* type = argv[1];
+	char* port = "7006";
+	
+	if (argc == 3){	//is port specified?
+		port = argv[2];
+	}
+	
+	//declarations
+	int sockfd, s;
+	struct addrinfo hints, *result;
+	char received[50];
+	
+	memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; 
+    hints.ai_socktype = SOCK_STREAM;
+	
+	//get IP address from hostname
+    if ((s = getaddrinfo(argv[1], port, &hints , &result)) != 0) 
+    {
+        perror("Error getting dns");
+        exit(1);
+    }
+	
+	//create socket
+	if ((sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == -1)
+    {
+		perror("Socket call failed");
+		exit(1);
+    }
+	
+	//connect socket
+	if (connect(sockfd, result->ai_addr, result->ai_addrlen) == -1)
+	{
+		perror("Connect call failed");
+		exit(1);
+	}
+	freeaddrinfo(result);
+	
+	printf("Waiting for READY...\n");
+	if (recv(sockfd, &received, sizeof(received), 0) < 0){
+		printf("Unable to read data from server\n");
+	}
+	printf("%s received\n\n", received);
+	
+	char keepPlaying = 'y';
+	
+	while (keepPlaying == 'y'){  //game loop
+		char *toSend[50];
+		
+		getNickName(sockfd); //gets a unique nickname
+		
+		//waiting for "GO"
+		if (recv(sockfd, &received, sizeof(received), 0) < 0){
+			printf("Unable to read data from server\n");
+		}
+		printf("received (looking for GO): %s!\n", received);
+		
+		//keep running until "SCORE" is received
+		while (strcmp(received, "SCORE") != 0){
+			getRPS(sockfd);	//gets and sends an inputted move to server
+			
+			//get response from server, where "SCORE" will stop game
+			if (recv(sockfd, &received, sizeof(received), 0) < 0){
+				printf("Unable to read data from server\n");
+			}
+			printf("received: %s\n", received);
+		}
+		keepPlaying = 'n';
+		/*printf("Would you like to play again? (y/n): ");
+		scanf("%c", toSend);
+		
+		if (send(sockfd, toSend, 1, 0) < 0){
+			printf("Unable to send data to server\n");
+		}
+		
+		//receives char for keepPlaying, 'y' being to keep going, 'n' to stop
+		if (recv(sockfd, &keepPlaying, 1, 0) < 0){
+			printf("Unable to read data from server\n");
+		}*/
+		//printf("Received from server: %s\n", received);
+	}
 	close(sockfd);
-
-	if (wait(NULL) < -1)
-		perror("Error in wait: ");
 	
-	exit(0);
-} //end main
+	return 0;
+}
